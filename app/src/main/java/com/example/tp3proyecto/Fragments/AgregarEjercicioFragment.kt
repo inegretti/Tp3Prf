283a1629795710ff67fb2b81ba38613d8c4c0e4b
inegretti <88805043+inegretti@users.noreply.github.com>
2023-09-16 14:56:23 -0300

    decimo segunda notas: Se agrega la opcion de agregar ejercicios en la rutina del usuario( para el admin).

--- app/src/main/java/com/example/tp3proyecto/Fragments/AgregarEjercicioFragment.kt
+++ app/src/main/java/com/example/tp3proyecto/Fragments/AgregarEjercicioFragment.kt
@@ -6,22 +6,81 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.tp3proyecto.Entidades.Ejercicio
 import com.example.tp3proyecto.R
+import com.example.tp3proyecto.Repository.Repositorio
+import com.google.android.material.snackbar.Snackbar
 
 class AgregarEjercicioFragment : Fragment() {
 
 
 
     private lateinit var v: View
-
+    private lateinit var spin:Spinner
+    private lateinit var ingRep:TextView
+    private lateinit var ingSer:TextView
+    private lateinit var btn:Button
+    var rep:Repositorio = Repositorio()
+    //var z=AgregarEjercicioFragmentArgs.fromBundle(requireArguments())
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         v= inflater.inflate(R.layout.fragment_agregar_ejercicio, container, false)
+        spin=v.findViewById(R.id.spinnerEj)
+        ingRep=v.findViewById(R.id.ingRep)
+        ingSer=v.findViewById(R.id.ingSer)
+        btn = v.findViewById(R.id.btnAAR)
+
+
+        var op:MutableList<String> = mutableListOf()
+        for (ejercicio in rep.listaE) {
+            op.add(ejercicio.nombre)
+        }
+        var  adapter:ArrayAdapter<String>
+        adapter = ArrayAdapter<String>(requireActivity(),android.R.layout.simple_spinner_item,op)
+        spin.adapter=adapter
         return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        var z = AgregarEjercicioFragmentArgs.fromBundle(requireArguments())
+        btn.setOnClickListener() {
+            if (ingSer.text.isEmpty() || ingSer.text.isEmpty()) {
+                Snackbar.make(
+                    v,
+                    "los datos no pueden ser vacios o menores a 0",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            } else {
+                var obj = rep.listaE.find { it.nombre == spin.selectedItem.toString() }
+                if (obj != null) {
+                    obj.series = Integer.parseInt(ingSer.text.toString())
+                    obj.repeticiones = Integer.parseInt(ingRep.text.toString())
+                    z.usuario.semana[z.posicion].rutina.add(obj)
+                    Snackbar.make(v, "Se ha agregado el ejercicio", Snackbar.LENGTH_LONG).show()
+                    findNavController().navigateUp()
+                } else {
+                    Snackbar.make(
+                        v,
+                        "no se ha encontrado un ejercicio con ese nombre",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+
+            }
+
+
+        }
+    }
     }
 
 
 
-}
--- app/src/main/java/com/example/tp3proyecto/Fragments/RutinaDetail.kt
+++ app/src/main/java/com/example/tp3proyecto/Fragments/RutinaDetail.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,6 +22,7 @@ class RutinaDetail : Fragment() {
     private lateinit var t:TextView
     private lateinit var r:RecyclerView
     private lateinit var ejAdap:AdapterEjercicio
+    private lateinit var btn:Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,16 +30,24 @@ class RutinaDetail : Fragment() {
         v=  inflater.inflate(R.layout.fragment_rutina_detail, container, false)
         t=v.findViewById(R.id.tituloRutinaDetail)
         r=v.findViewById(R.id.rutinaDet)
+        btn=v.findViewById(R.id.btnAGR)
+
         return v
     }
 
     override fun onStart() {
         super.onStart()
         var z=RutinaDetailArgs.fromBundle(requireArguments())
+        if(z.clearance){
+            btn.setVisibility(View.VISIBLE)
+
+        }else{
+            btn.setVisibility(View.INVISIBLE)
+        }
         t.text=z.usuario.semana[z.posicion].nombre
         ejAdap=AdapterEjercicio(z.usuario.semana[z.posicion].rutina){position->
             val rut = z.usuario.semana[position]
-            Snackbar.make(v, "prueba ${rut.rutina.get(0).nombre}", Snackbar.LENGTH_LONG).show()
+            //Snackbar.make(v, "prueba ${rut.rutina.get(0).nombre}", Snackbar.LENGTH_LONG).show()
             var ej= z.usuario.semana[z.posicion].rutina[position]
 
             val action =  RutinaDetailDirections.actionRutinaDetailToEjercicioDetail(z.usuario,z.clearance,ej,z.posicion)
@@ -45,7 +55,10 @@ class RutinaDetail : Fragment() {
         }
         r.layoutManager= LinearLayoutManager(context)
         r.adapter=ejAdap
-
+        btn.setOnClickListener() {
+            val action = RutinaDetailDirections.actionRutinaDetailToAgregarEjercicioFragment2(z.usuario,z.posicion)
+            findNavController().navigate(action)
+        }
     }
 
 }
