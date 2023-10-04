8d947a4b9a29717bac2e8e381bad474ac0091ebb
Jplevet <juann.levet@hotmail.com>
2023-10-03 21:42:01 -0300

    Se declararon las pantallas, los findViewById y la bottomBar

--- app/src/main/java/com/example/tp3proyecto/MainActivity.kt
+++ app/src/main/java/com/example/tp3proyecto/MainActivity.kt
@@ -3,12 +3,23 @@ package com.example.tp3proyecto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.tp3proyecto.Entidades.Usuario
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        bottomNavigationView = findViewById(R.id.bottom_bar)
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
 
 
     }
