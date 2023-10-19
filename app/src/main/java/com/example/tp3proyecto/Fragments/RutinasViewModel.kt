617b8447601e1906087440bfad4a8bca4de84517
Jplevet <juann.levet@hotmail.com>
2023-10-19 19:19:40 -0300

    Se modifico para que tome los datos del usuario singleton

--- app/src/main/java/com/example/tp3proyecto/Fragments/RutinasViewModel.kt
+++ app/src/main/java/com/example/tp3proyecto/Fragments/RutinasViewModel.kt
@@ -1,12 +1,30 @@
 package com.example.tp3proyecto.Fragments
 
 import androidx.lifecycle.ViewModel
+import com.example.tp3proyecto.Entidades.Usuario
+import com.example.tp3proyecto.Entidades.UsuarioSingleton
 
 class RutinasViewModel : ViewModel() {
-    lateinit var z:RutinasFragmentArgs
-    // TODO: Implement the ViewModel
+    private lateinit var z: RutinasFragmentArgs
+    private val usuarioSingleton : UsuarioSingleton
 
-    fun tieneRutinas(pos:Int):Boolean{
+    init {
+        usuarioSingleton = UsuarioSingleton
+    }
+    fun setZ(rutinasFragmentArgs: RutinasFragmentArgs) {
+        z = rutinasFragmentArgs
+    }
+
+    fun getZ(): RutinasFragmentArgs {
+        return z
+    }
+
+    fun tieneRutinas(pos: Int): Boolean {
         return z.usuario.semana[pos].rutina.isEmpty()
     }
+
+    fun getInstance(): Usuario {
+        return usuarioSingleton.getInstance()
+    }
 }
+
