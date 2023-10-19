2b288d2154f7c440b90ef0c8e79163d2fb8f3155
Jplevet <juann.levet@hotmail.com>
2023-10-19 19:21:47 -0300

    Se modifico para que agregar la bottomBar navigation

--- app/src/main/java/com/example/tp3proyecto/MainActivity.kt
+++ app/src/main/java/com/example/tp3proyecto/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.tp3proyecto
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.tp3proyecto.Entidades.Usuario
+import com.example.tp3proyecto.Entidades.UsuarioSingleton
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -13,20 +17,40 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var navHostFragment: NavHostFragment
+    private lateinit var usuario: Usuario
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        bottomNavigationView = findViewById(R.id.bottom_bar)
-        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+        val navController = navHostFragment.navController
 
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_bar)
 
+        bottomNavigationView.setupWithNavController(navController)
+
+//        bottomNavigationView.setOnItemSelectedListener { item ->
+//            when(item.itemId){
+//                R.id.nav_logout -> {
+//                    val builder = AlertDialog.Builder(this)
+//                    builder.setTitle("Cerrar sesión?")
+//                    builder.setPositiveButton("Sí", DialogInterface.OnClickListener { dialog, which ->
+//                        usuario = usuario ?: null
+//                        navController.navigate(R.id.loginFragment)
+//                    })
+//                    builder.setNegativeButton("No", DialogInterface.OnClickListener { dialog, which ->
+//
+//                    })
+//                    builder.show()
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
-
     override fun onStart() {
         super.onStart()
-
     }
 
 }
