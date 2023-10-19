1e2f19d12afe91e467e870b85e94f0a231c35652
Jplevet <juann.levet@hotmail.com>
2023-10-19 19:20:34 -0300

    Se agrego la visibilidad de la bottombar

--- app/src/main/java/com/example/tp3proyecto/Fragments/UserFragment.kt
+++ app/src/main/java/com/example/tp3proyecto/Fragments/UserFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.example.tp3proyecto.Entidades.UsuarioSingleton
 import com.example.tp3proyecto.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
@@ -16,9 +18,9 @@ class UserFragment : Fragment() {
 
     lateinit var v:View
     lateinit var txt: TextView
-    lateinit var btnRut:Button
-    lateinit var btnCon:Button
-
+//    lateinit var btnRut:Button
+//    lateinit var btnCon:Button
+    lateinit var bottomNavigationView : BottomNavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +28,8 @@ class UserFragment : Fragment() {
     ): View? {
         v= inflater.inflate(R.layout.fragment_user, container, false)
         txt=v.findViewById(R.id.txtusuario)
-        btnRut=v.findViewById(R.id.btnRut)
-        btnCon=v.findViewById(R.id.btnConfig)
+//        btnRut=v.findViewById(R.id.btnRut)
+//        btnCon=v.findViewById(R.id.btnConfig)
 
         val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottom_bar)
         bottomNavigationView?.visibility = View.VISIBLE
@@ -37,21 +39,11 @@ class UserFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        var z = UserFragmentArgs.fromBundle(requireArguments())
-        txt.text="Bienvenido ${z.usuario.name} }"
-        btnRut.setOnClickListener() {
-            //Snackbar.make(v, "proximamente", Snackbar.LENGTH_LONG).show()
+//        var z = UserFragmentArgs.fromBundle(requireArguments())
+//        txt.text="Bienvenido ${z.usuario.name} }"
+        var usuario = UsuarioSingleton.getInstance()
+        txt.text = "Bienvenido, ${usuario.name}!"
 
-            // esto es una pagina de transicion por ello no seria necesario el tener que pasar los datos al view model
-            val action =  UserFragmentDirections.actionUserFragmentToRutinasFragment(z.usuario,false)
-            findNavController().navigate(action)
-            Snackbar.make(v, "En construccion", Snackbar.LENGTH_LONG).show()
-        }
-        btnCon.setOnClickListener(){
-
-            val action =  UserFragmentDirections.actionUserFragmentToConfigFragment(z.usuario)
-            findNavController().navigate(action)
-        }
     }
 
 }
