7501f3a3958da01179268b4d1047a0b5ace5ff93
Jplevet <juann.levet@hotmail.com>
2023-10-21 02:28:04 -0300

    Esta clase se elimino

--- app/src/main/java/com/example/tp3proyecto/Entidades/DatoUsuario.kt
+++ app/src/main/java/com/example/tp3proyecto/Entidades/DatoUsuario.kt
@@ -1,6 +1,9 @@
 package com.example.tp3proyecto.Entidades
 
-data class Notificacion (
-    var titulo : String,
-    var descripcion : String
+data class DatoUsuario (
+    var name:String,
+    var password:String,
+    var Email:String,
+    var pesoActual:Double,
+    var altura:Double
 )
