7c62f8fed5f45ab44388c36e6484bca6712e738d
Jplevet <juann.levet@hotmail.com>
2023-10-21 02:27:45 -0300

    Se agrego la linea de codigo para almacenar los datos del usuario en el singleton

--- app/src/main/java/com/example/tp3proyecto/Fragments/LoginFragment.kt
+++ app/src/main/java/com/example/tp3proyecto/Fragments/LoginFragment.kt
@@ -9,6 +9,8 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.tp3proyecto.Entidades.Usuario
+import com.example.tp3proyecto.Entidades.UsuarioSingleton
 import com.example.tp3proyecto.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
@@ -19,8 +21,10 @@ class LoginFragment : Fragment() {
     lateinit var email: EditText
     lateinit var contraseña: EditText
     lateinit var btn: Button
-    //var lista2:Repositorio=Repositorio()
     private lateinit var viewModel: LoginViewModel
+
+    private lateinit var usuario: Usuario
+    private lateinit var usuarioSingleton : UsuarioSingleton
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -42,18 +46,8 @@ class LoginFragment : Fragment() {
         val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottom_bar)
         bottomNavigationView?.visibility = View.GONE
 
-
         return v
     }
-
-
-/*
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(BlankViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-*/
     override fun onStart() {
         super.onStart()
         btn.setOnClickListener() {
@@ -65,53 +59,21 @@ class LoginFragment : Fragment() {
 
                 if(viewModel.validarPass(contraseña.text.toString())){
 
+                    UsuarioSingleton.setUsuario(viewModel.usuario)
+
                     if(viewModel.usuario.name=="Admin"){
                         val action = LoginFragmentDirections.actionLoginFragmentToAdminFragment(viewModel.lista2)
                         findNavController().navigate(action)
                     }else{
                         val action = LoginFragmentDirections.actionLoginFragmentToUserFragment(viewModel.usuario)
                         findNavController().navigate(action)
-//                        val action = LoginFragmentDirections.actionLoginFragmentToUserFragment()
-//                        findNavController().navigate(action)
                     }
-
-
-//                    Snackbar.make(v, "Bienvenido ${viewModel.usuario.name}", Snackbar.LENGTH_LONG).show()
                 }else{
                     Snackbar.make(v, "contraseña incorrecta", Snackbar.LENGTH_LONG).show()
                 }
-                /*
-                if(contraseña.text.toString() == usuario.password){
-
-                    if(usuario.name=="Admin"){
-                        val action = LoginFragmentDirections.actionLoginFragmentToAdminFragment(lista2)
-                        Log.d("hola= ",lista2.toString())
-                        findNavController().navigate(action)
-                        // findNavController().navigateUp()
-
-                    }else{
-                        val action = LoginFragmentDirections.actionLoginFragmentToUserFragment(usuario)
-                        findNavController().navigate(action)
-                    }
-
-
-                    Snackbar.make(v, "Bienvenido ${usuario.name}", Snackbar.LENGTH_LONG).show()
-                }else{
-                    Snackbar.make(v, "Contraseña incorrecta", Snackbar.LENGTH_LONG).show()
-                }
-                */
-
-
             }else{
                 Snackbar.make(v, "No existe un usuario registrado con ese mail", Snackbar.LENGTH_LONG).show()
             }
-
-
         }
-
-
-
-
-        }
-
     }
+}
