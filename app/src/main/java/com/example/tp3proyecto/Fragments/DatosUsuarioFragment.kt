351b52e96cfb6f1a96988710a9abbba37e25cb16
Jplevet <juann.levet@hotmail.com>
2023-10-21 02:47:14 -0300

    Se agrego la funcionalidad de cerrar sesion

--- app/src/main/java/com/example/tp3proyecto/Fragments/DatosUsuarioFragment.kt
+++ app/src/main/java/com/example/tp3proyecto/Fragments/DatosUsuarioFragment.kt
@@ -1,13 +1,16 @@
 package com.example.tp3proyecto.Fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.tp3proyecto.Entidades.Usuario
 import com.example.tp3proyecto.Entidades.UsuarioSingleton
 import com.example.tp3proyecto.R
@@ -21,6 +24,7 @@ class DatosUsuarioFragment : Fragment() {
     lateinit var pesoUsuario : TextView
     lateinit var alturaUsuario : TextView
     lateinit var imagenUsuario : ImageView
+    lateinit var cerrarSesionBoton : Button
 
     private lateinit var viewModel: DatosUsuarioViewModel
     private lateinit var usuario: Usuario
@@ -40,6 +44,7 @@ class DatosUsuarioFragment : Fragment() {
         alturaUsuario = v.findViewById(R.id.alturaUsuario)
         imagenUsuario = v.findViewById(R.id.imagenUsuario)
         imagenUsuario.setImageResource(R.drawable.perfil)
+        cerrarSesionBoton = v.findViewById(R.id.cerrarSesionBoton)
 
         val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottom_bar)
         bottomNavigationView?.visibility = View.VISIBLE
@@ -54,5 +59,49 @@ class DatosUsuarioFragment : Fragment() {
         mailUsuario.text = "${usuario.Email}"
         pesoUsuario.text = "${usuario.pesoActual}"
         alturaUsuario.text = "${usuario.altura}"
+
+        cerrarSesionBoton.setOnClickListener {
+
+//            usuario = Usuario(
+//                id = 0,
+//                name = "Nombre",
+//                password = "Contraseña",
+//                Email = "email@example.com",
+//                imgUrl = "URL de la imagen",
+//                pesoActual = 0.0,
+//                altura = 0.0
+//            )
+//            UsuarioSingleton.setUsuario(usuario)
+//
+//            val action = DatosUsuarioFragmentDirections.actionDatosUsuarioFragmentToLoginFragment(usuario)
+//            findNavController().navigate(action)
+
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Cerrar sesión")
+            builder.setMessage("¿Está seguro que desea cerrar sesión?")
+
+            builder.setPositiveButton("Sí") { dialog, which ->
+                usuario = Usuario(
+                    id = 0,
+                    name = "Nombre",
+                    password = "Contraseña",
+                    Email = "email@example.com",
+                    imgUrl = "URL de la imagen",
+                    pesoActual = 0.0,
+                    altura = 0.0
+                )
+                UsuarioSingleton.setUsuario(usuario)
+
+                val action = DatosUsuarioFragmentDirections.actionDatosUsuarioFragmentToLoginFragment(usuario)
+                findNavController().navigate(action)
+            }
+
+            builder.setNegativeButton("No") { dialog, which ->
+                dialog.dismiss()
+            }
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
     }
 }
