3b1af1077479bff0022ae7fabf322d4b898a3c2e
Jplevet <juann.levet@hotmail.com>
2023-10-21 02:46:20 -0300

    Se almacenan los datos del usuario al usuario singleton

--- app/src/main/java/com/example/tp3proyecto/Fragments/ConfigViewModel.kt
+++ app/src/main/java/com/example/tp3proyecto/Fragments/ConfigViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.tp3proyecto.Fragments
 
 import androidx.lifecycle.ViewModel
-
+import com.example.tp3proyecto.Entidades.Usuario
+import com.example.tp3proyecto.Entidades.UsuarioSingleton
 class ConfigViewModel : ViewModel() {
-    lateinit var z:ConfigFragmentArgs
-    // TODO: Implement the ViewModel
 
+    private val usuarioSingleton = UsuarioSingleton
 
-
+    fun getInstance(): Usuario {
+        return usuarioSingleton.getInstance()
+    }
 }
